ee84e8f74b1ea26d16c47a8a7ebf575d73f4f762
Mike Rew <enderman.mike@gmail.com>
2015-01-03 10:32:35 -0800

    start of server, package.json

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+router.get('/', function(req, res){
+	//res.json({ message: 'Something goes here!'});
+	res.send('Hello there world');
+});
+
+app.use('/api', router);
+app.listen(port);
+console.log('Listening on port 3000');
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(port);
