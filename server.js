9982dcabe8f075d95f66bdb089b85a14d6983150
Mike Rew <enderman.mike@gmail.com>
2015-01-03 11:17:30 -0800

    item post and items

--- server.js
+++ server.js
@@ -6,15 +6,47 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require('mongoose');
-var item = require('./models/item');
+var Item = require('./models/item');
 mongoose.connect('mongodb://wpdba:lim1t<$Plz@ds045097.mongolab.com:45097/wpostdb');
 
 var port = process.env.PORT || 3000;
 var router = express.Router();
+var itemRoute = router.route('/item');
+var itemsRoute = router.route('/items');
 
 router.get('/', function(req, res){
 	//res.json({ message: 'Something goes here!'});
-	res.send('Hello there world');
+	res.send('Here we go!');
+});
+
+itemRoute.post(function(req, res){
+	var item = new Item();
+
+	item.title = req.body.title;
+	item.desc = req.body.desc;
+	item.price = req.body.price;
+	item.viewed = 0;
+	item.userid = 'mike';
+	item.catid = 'stupid';
+
+
+	item.save(function(err){
+		if(err){
+			res.send(err);
+		}else{
+			res.json({ item: item });
+		}
+	});
+});
+
+itemsRoute.get(function(req, res){
+	Item.find(function(err, items){
+		if(err){
+			res.send(err);
+		}else{
+			res.json(items);
+		}
+	});
 });
 
 app.use('/api', router);
